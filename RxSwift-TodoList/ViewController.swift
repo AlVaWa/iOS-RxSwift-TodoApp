cb6a8dd8d12ca13724237d86c6b1543635c2c12a
Aleksander Waage <aleksander.waage@tv2.no>
2020-07-03 09:20:37 +0200

    Initial Commit

---
+++ RxSwift-TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwift-TodoList
+//
+//  Created by Aleksander Waage on 03/07/2020.
+//  Copyright © 2020 Waageweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
